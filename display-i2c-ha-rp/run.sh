c90ffbcaf913311d463ee06abc80c7df04d0d2d8
HuskyDevOps <1150gsler@gmail.com>
2026-01-18 12:06:14 +0100

    Delete display-i2c-ha-rp/run.sh

--- display-i2c-ha-rp/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-echo "--- I2C Bus Scan ---"
-# Scannt den Bus 1 und gibt die Matrix im Log aus
-i2cdetect -y 1 || echo "I2C-Tools nicht gefunden oder Bus belegt"
-
-echo "Starte I2C Display Skript..."
-cd /app
-python3 display.py
